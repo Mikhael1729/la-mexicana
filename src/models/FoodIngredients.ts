b2e87650d99329c99a9f6fb506a98f9d8c2268aa
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2018-10-21 21:28:25 -0700

    refactor: Some changes in models.

--- src/models/FoodIngredients.ts
+++ src/models/FoodIngredients.ts
@@ -2,10 +2,17 @@ import { IBaseEntity } from "./IBaseEntity";
 import { Food } from "./Food";
 import { Ingredient } from "./Ingredient";
 import { IngredientsGroup } from "./IngredientsGroup";
+import { init } from "helpers/Init";
 
 export interface IFoodIngredients extends IBaseEntity{
     food: Food
     ingredients: Ingredient[];
     ingredientsGroup: IngredientsGroup[];
 }
+export interface FoodIngredients extends IFoodIngredients { }
 
+export class FoodIngredients implements FoodIngredients {
+    public constructor(foodIngredients: IFoodIngredients) {
+        init<IFoodIngredients>(this, foodIngredients);
+    }
+} 
---
+++ src/models/index.ts
@@ -0,0 +1,13 @@
+import { Food } from './Food';
+import { IFoodIngredients } from './FoodIngredients';
+import { Ingredient } from './Ingredient';
+import { IBaseEntity } from './IBaseEntity';
+import { IngredientsGroup } from './IngredientsGroup';
+
+export {
+    Food, 
+    IFoodIngredients, 
+    Ingredient, 
+    IBaseEntity, 
+    IngredientsGroup,
+}
